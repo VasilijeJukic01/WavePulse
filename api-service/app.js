df7c0ac06412e344d6c17d65987b3fddf20f1013
Vasilije Jukic <vasilijejukic@gmail.com>
2024-04-05 23:16:01 +0200

    Tested GET routes

--- api-service/app.js
+++ api-service/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { sequelize } = require("./models");
 const app = express();
 
 app.get('/', (req, res) => {
@@ -7,15 +6,30 @@ app.get('/', (req, res) => {
 });
 
 const routes = {
-
+    '/album': require('./routes/album.js'),
+    '/albumrating': require('./routes/albumrating.js'),
+    '/albumreview': require('./routes/albumreview.js'),
+    '/artist': require('./routes/artist.js'),
+    '/concert': require('./routes/concert.js'),
+    '/country': require('./routes/country.js'),
+    '/follow': require('./routes/follow.js'),
+    '/genre': require('./routes/genre.js'),
+    '/playlist': require('./routes/playlist.js'),
+    '/playlistsong': require('./routes/playlistsong.js'),
+    '/role': require('./routes/role.js'),
+    '/song': require('./routes/song.js'),
+    '/songgenre': require('./routes/songgenre.js'),
+    '/songrating': require('./routes/songrating.js'),
+    '/songreview': require('./routes/songreview.js'),
+    '/songtag': require('./routes/songtag.js'),
+    '/tag': require('./routes/tag.js'),
+    '/user': require('./routes/user.js'),
+    '/usersettings': require('./routes/usersettings.js'),
+    'concertartist': require('./routes/concertartist.js')
 };
 
 Object.entries(routes).forEach(([path, route]) => {
     app.use(path, route);
 });
 
-app.listen(8000, async () => {
-    console.log('Server started on localhost:8000');
-    await sequelize.sync({ force: false });
-    console.log('DB synced');
-});
+module.exports = app;
